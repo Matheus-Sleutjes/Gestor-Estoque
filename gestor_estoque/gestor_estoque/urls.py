3b80849b00341fe374473871cdb883167064d03f
Matheus-Sleutjes <sleutjes13@gmail.com>
2025-01-06 19:01:20 -0400

    base projeto python

---
+++ gestor_estoque/gestor_estoque/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+from front import views
+
+urlpatterns = [
+    path('', views.home, name='home'),
+    path('login/', views.login, name='login'),
+    # path('', views.home, name='home'),
+
+]
